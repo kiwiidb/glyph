e09a38cc8080cae8ebfb76e27f7fe04949af31f7
kiwiidb <kwinten.debacker@hotmail.com>
2023-02-09 17:15:12 +0100

    add nostr client

--- lib/controllers/main_controller.dart
+++ lib/controllers/main_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -7,16 +8,24 @@ import 'package:convert/convert.dart';
 import 'package:dart_bech32/dart_bech32.dart';
 import 'package:glyph/models/utxo.dart';
 import 'package:http/http.dart' as http;
+import 'package:nostr/nostr.dart';
 
 class MainControlller extends GetxController {
   final TextEditingController pubkeyController = TextEditingController();
   var utxos = List<Utxo>.empty().obs;
   var loading = true.obs;
 
+  @override
+  void onInit() {
+    fetchNostrFollows("dummy");
+    super.onInit();
+  }
+
   String getAddressFromPubkey(String pubkey) {
     //damn I don't know what I'm doing
     var decoded = bech32.decode(pubkey);
     var newList = List<int>.from(decoded.words);
+    //add segwit version (1: taproot)
     newList.insert(0, 1);
     var newWords = Uint8List.fromList(newList);
     var encoded = bech32m.encode(Decoded(prefix: "bc", words: newWords));
@@ -42,6 +51,39 @@ class MainControlller extends GetxController {
 
   String getImageUrl(Utxo utxo) {
     return 'https://ordinals.com/content/${utxo.txid}i${utxo.vout}';
+  }
+
+  void fetchNostrFollows(String pubkey) async {
+// Create a subscription message request with one or many filters
+    Request requestWithFilter = Request("sdfsdfsdfs", [
+      Filter(
+        p: ["8c3b267e9db6b0115498cc3efcd187d1474864940ae8ff977826b9d83d205877"],
+        kinds: [3],
+        limit: 500,
+      )
+    ]);
+
+    // Connecting to a nostr relay using websocket
+    WebSocket webSocket = await WebSocket.connect(
+      'wss://brb.io', // or any nostr relay
+    );
+    // if the current socket fail try another one
+    // wss://nostr.sandwich.farm
+    // wss://relay.damus.io
+
+    // Send a request message to the WebSocket server
+    webSocket.add(requestWithFilter.serialize());
+
+    // Listen for events from the WebSocket server
+    print("started listening..");
+    await Future.delayed(Duration(seconds: 1));
+    webSocket.listen((event) {
+      var parsed = Message.deserialize(event) as Event;
+      print('Received event: ${parsed.tags}');
+    });
+
+    // Close the WebSocket connection
+    await webSocket.close();
   }
 }
 
